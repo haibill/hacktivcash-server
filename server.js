5cc8f7f36d78be850f1cd76c5f8bfc5e1a674fd8
Riza Fahmi <rizafahmi@gmail.com>
2017-04-01 06:05:48 +0700

    Finish extending example

--- server.js
+++ server.js
@@ -19,8 +19,15 @@ const handleGetRoot = (request, response) => {
 const handleGetEcho = (request, response) => {
   response.send(`This is /echo route. Hello ${request.params.name}`)
 }
+
 app.get('/', handleGetRoot)
 app.get('/echo/:name', handleGetEcho)
+app.get('/hacktiv8', (req, res) => {
+  res.redirect('https://hacktiv8.com/')
+})
+app.get('/sending', (req, res) => {
+  res.sendFile('/Users/riza/Music/cool_song.mp3')
+})
 
 app.listen(5432, () => {
   console.log('Magic happen at http://localhost:5432')
