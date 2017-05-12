431998ab9595277c325d682431f3e55656ab98d1
Riza Fahmi <rizafahmi@gmail.com>
2017-05-12 13:33:07 +0700

    Bulk insert

--- server.js
+++ server.js
@@ -14,14 +14,14 @@ app.set('view engine', 'hbs')
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.render('index', {items: items})
+  res.render('index', { items: items })
 })
 
 app.get('/detail/:id', (req, res) => {
   const item = items.find(i => {
     return i.id === parseInt(req.params.id)
   })
-  res.render('detail', {item: item})
+  res.render('detail', { item: item })
 })
 
 app.listen(5432, () => {
@@ -33,6 +33,7 @@ image VARCHAR(100),
 description TEXT);`
 const DROP_TABLE = `DROP TABLE IF EXISTS items;`
 const INSERT_ITEM = `INSERT INTO items (id, image, description) VALUES (1, "/img/1-jlol_sp_ed_bb-8_sphero_force_band.jpg", "abc");`
+const INSERT_ITEM_STMT = `INSERT INTO items (id, image, description) VALUES (?, ?, ?)`
 const SELECT_ITEM = `SELECT * FROM items;`
 const UPDATE_ITEM = `UPDATE items SET description='HACKTIVCAST new items'`
 const DELETE_ITEM = `DELETE FROM items WHERE id=1;`
@@ -40,7 +41,11 @@ const DELETE_ITEM = `DELETE FROM items WHERE id=1;`
 db.serialize(() => {
   db.run(DROP_TABLE)
   db.run(CREATE_TABLE)
-  db.run(INSERT_ITEM)
+  const stmt = db.prepare(INSERT_ITEM_STMT)
+  for (let i = 1; i <= 10; i++) {
+    stmt.run(i, `/img/${i}`, `Item #${i}`)
+  }
+  stmt.finalize()
 
   db.each(SELECT_ITEM, (err, row) => {
     console.log(row.id, ': ', row.description)
