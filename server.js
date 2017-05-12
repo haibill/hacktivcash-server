618079668741ecd8cb8b64989470c961ee738948
Riza Fahmi <rizafahmi@gmail.com>
2017-05-12 11:37:04 +0700

    DDL using Node and SQLite

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const path = require('path')
 const express = require('express')
 const items = require('./data/items.json')
+const sqlite3 = require('sqlite3').verbose()
 
 const app = express()
+const db = new sqlite3.Database('./data/try.db')
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
@@ -25,3 +27,17 @@ app.get('/detail/:id', (req, res) => {
 app.listen(5432, () => {
   console.log('Magic happen at http://localhost:5432')
 })
+
+const CREATE_TABLE = `CREATE TABLE users (id INTEGER PRIMARY KEY AUTOINCREMENT, 
+username VARCHAR(100),
+password TEXT);`
+const ALTER_TABLE = `ALTER TABLE users ADD dateCreated DATE;`
+const DROP_TABLE = `DROP TABLE users;`
+
+db.serialize(() => {
+  db.run(CREATE_TABLE)
+  db.run(ALTER_TABLE)
+  db.run(DROP_TABLE)
+})
+
+db.close()
