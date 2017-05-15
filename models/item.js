a2bc415feafbc0f7c8cac338285e8be3fce1a78a
Riza Fahmi <rizafahmi@gmail.com>
2017-05-15 10:07:45 +0700

    Generate Item model

---
+++ models/item.js
@@ -0,0 +1,14 @@
+'use strict'
+module.exports = function (sequelize, DataTypes) {
+  var Item = sequelize.define('Item', {
+    image: DataTypes.STRING,
+    description: DataTypes.TEXT
+  }, {
+    classMethods: {
+      associate: function (models) {
+        // associations can be defined here
+      }
+    }
+  })
+  return Item
+}
