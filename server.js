511a6607c7744f79a1da81556d36f2f8df1b119c
Riza Fahmi <rizafahmi@gmail.com>
2017-05-15 10:15:36 +0700

    Syncing the database with schema

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const path = require('path')
 const express = require('express')
-const sqlite3 = require('sqlite3').verbose()
 
 const app = express()
-const db = new sqlite3.Database('./data/try.db')
 
 const controllers = require('./controllers')
+const sequelize = require('./models').sequelize
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
@@ -18,42 +17,10 @@ app.use(express.static('public'))
 app.get('/', controllers.index)
 app.get('/detail/:id', controllers.details)
 
-app.listen(5432, () => {
-  console.log('Magic happen at http://localhost:5432')
-})
-
-// MODELS
-
-const CREATE_TABLE = `CREATE TABLE IF NOT EXISTS items(id INTEGER PRIMARY KEY AUTOINCREMENT, 
-image VARCHAR(100),
-description TEXT);`
-const DROP_TABLE = `DROP TABLE IF EXISTS items;`
-const INSERT_ITEM = `INSERT INTO items (id, image, description) VALUES (1, "/img/1-jlol_sp_ed_bb-8_sphero_force_band.jpg", "abc");`
-const INSERT_ITEM_STMT = `INSERT INTO items (id, image, description) VALUES (?, ?, ?)`
-const SELECT_ITEM = `SELECT * FROM items;`
-const UPDATE_ITEM = `UPDATE items SET description='HACKTIVCAST new items'`
-const DELETE_ITEM = `DELETE FROM items WHERE id=1;`
-
-db.serialize(() => {
-  db.run(DROP_TABLE)
-  db.run(CREATE_TABLE)
-  const stmt = db.prepare(INSERT_ITEM_STMT)
-  for (let i = 1; i <= 10; i++) {
-    stmt.run(i, `/img/${i}`, `Item #${i}`)
-  }
-  stmt.finalize()
-
-  db.each(SELECT_ITEM, (err, row) => {
-    console.log(row.id, ': ', row.description)
+sequelize.sync()
+  .then(() => {
+    app.listen(5432, () => {
+      console.log('Magic happen at http://localhost:5432')
+    })
   })
-  db.run(UPDATE_ITEM)
-  db.each(SELECT_ITEM, (err, row) => {
-    console.log(row.id, ': ', row.description)
-  })
-  db.run(DELETE_ITEM)
-  db.each(SELECT_ITEM, (err, row) => {
-    console.log('After DELETE: ', row.id, ': ', row.description)
-  })
-})
-
-db.close()
+  .catch(err => console.error('Something happen with sync(). Error message: ', err))
